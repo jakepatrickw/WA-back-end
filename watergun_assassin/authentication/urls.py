9855ee729f8b69ae0291a066cc91416c9d3ce873
[jacob <jakepwalsh@gmail.com>
2020-05-31 15:53:49 -0700

    add new authentication app

---
+++ watergun_assassin/authentication/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views as authentication_views
+
+urlpatterns = [
+    
+    path('create/',authentication_views.create_user,name='create_user')
+    
+    ]

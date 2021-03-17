c037ebd4482aced6f47c53bda3a475cb5bff9fc0
[jacob <jakepwalsh@gmail.com>
2021-03-17 09:34:00 -0700

    starting new feature branch

--- watergun_assassin/game_logic/models.py
+++ watergun_assassin/game_logic/models.py
@@ -7,18 +7,18 @@ class Game(models.Model):
 
 
 class Player(models.Model):
-    player = models.OneToOneField(User, null=False, on_delete=models.CASCADE)
-    game = models.OneToOneField(Game, null=False, on_delete=models.CASCADE)
+    player = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, null=False, on_delete=models.CASCADE)
     is_admin = models.BooleanField(default=False)
 
 
-# class Round(models.Model):
-#     game = models.OneToOneField(Game, on_delete=models.CASCADE)
-#     round_number = models.IntegerField(default=0)
-#     assassinated = models.OneToOneField(Player, on_delete=models.CASCADE, null=True)
+class Round(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    round_number = models.IntegerField(default=0)
+    assassinated = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
 
 
-# class RoundPairing(models.Model):
-#     round_number = models.OneToOneField(Round, on_delete=models.CASCADE, null=False)
-#     assassin = models.OneToOneField(Player, null=True, on_delete=models.CASCADE, related_name='player')
-#     target = models.OneToOneField(Player, null=True, on_delete=models.CASCADE, related_name='target')
+class RoundPairing(models.Model):
+    round_number = models.ForeignKey(Round, on_delete=models.CASCADE, null=False)
+    assassin = models.ForeignKey(Player, null=True, on_delete=models.CASCADE)
+    target = models.ForeignKey(Player, null=True, on_delete=models.CASCADE, related_name='target')
